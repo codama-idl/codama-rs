746c58ac087a770f7afacdd398af9d81f3a99ded
Loris Leiva <loris.leiva@gmail.com>
2024-11-13 13:12:31 +0000

    wip

--- codama-nodes/src/type_nodes/amount_type_node.rs
+++ codama-nodes/src/type_nodes/amount_type_node.rs
@@ -27,20 +27,72 @@ impl AmountTypeNode {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Endian, NumberTypeNode, U64};
+    use crate::{
+        Endian, NestedTypeNodeTrait, NumberTypeNode, PostOffsetTypeNode, PreOffsetTypeNode, U64,
+    };
 
     #[test]
     fn new() {
+        let node = AmountTypeNode::new(NumberTypeNode::new(U64, Endian::Big), 0, None);
+        assert_eq!(
+            node.number,
+            NestedTypeNode::Value(NumberTypeNode::new(U64, Endian::Big))
+        );
+        assert_eq!(node.decimals, 0);
+        assert_eq!(node.unit, None);
+    }
+
+    #[test]
+    fn new_with_offset() {
         let node = AmountTypeNode::new(
             NumberTypeNode::new(U64, Endian::Big),
-            8,
+            9,
             Some("SOL".to_string()),
         );
         assert_eq!(
             node.number,
             NestedTypeNode::Value(NumberTypeNode::new(U64, Endian::Big))
         );
-        assert_eq!(node.decimals, 8);
+        assert_eq!(node.decimals, 9);
         assert_eq!(node.unit, Some("SOL".to_string()));
+    }
+
+    #[test]
+    fn new_with_explicit_value() {
+        let node = AmountTypeNode::new(
+            NestedTypeNode::Value(NumberTypeNode::new(U64, Endian::Big)),
+            0,
+            None,
+        );
+        assert_eq!(
+            node.number,
+            NestedTypeNode::Value(NumberTypeNode::new(U64, Endian::Big))
+        );
+    }
+
+    #[test]
+    fn new_with_nested_value() {
+        let node = AmountTypeNode::new(
+            PostOffsetTypeNode::pre_offset(
+                PreOffsetTypeNode::absolute(NumberTypeNode::new(U64, Endian::Big), 0),
+                0,
+            ),
+            9,
+            Some("SOL".to_string()),
+        );
+        assert_eq!(
+            node.number,
+            NestedTypeNode::PostOffset(Box::new(PostOffsetTypeNode::pre_offset(
+                NestedTypeNode::PreOffset(Box::new(PreOffsetTypeNode::absolute(
+                    NestedTypeNode::Value(NumberTypeNode::new(U64, Endian::Big)),
+                    0
+                ))),
+                0,
+            )))
+        );
+        assert_eq!(
+            node.number.get_nested_type_node(),
+            &NumberTypeNode::new(U64, Endian::Big)
+        );
     }
 }
