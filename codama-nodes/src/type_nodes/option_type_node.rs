fc2b73495c83b45dc9ba389d68850762f6d90ed4
Loris Leiva <loris.leiva@gmail.com>
2024-11-15 14:55:32 +0000

    wip

---
+++ codama-nodes/src/type_nodes/option_type_node.rs
@@ -0,0 +1,71 @@
+use super::{NestedTypeNode, NumberTypeNode, TypeNode, U8};
+use codama_nodes_derive::{Node, TypeNode};
+
+#[derive(Node, TypeNode, Debug, PartialEq)]
+pub struct OptionTypeNode {
+    // Data.
+    pub fixed: bool,
+
+    // Children.
+    pub item: TypeNode,
+    pub prefix: NestedTypeNode<NumberTypeNode>,
+}
+
+impl OptionTypeNode {
+    pub fn new<T>(item: T) -> Self
+    where
+        T: Into<TypeNode>,
+    {
+        Self {
+            fixed: false,
+            item: item.into(),
+            prefix: NumberTypeNode::le(U8).into(),
+        }
+    }
+
+    pub fn fixed<T>(item: T) -> Self
+    where
+        T: Into<TypeNode>,
+    {
+        Self {
+            fixed: true,
+            item: item.into(),
+            prefix: NumberTypeNode::le(U8).into(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{NumberTypeNode, StringTypeNode, U64};
+
+    #[test]
+    fn new() {
+        let node = OptionTypeNode::new(NumberTypeNode::le(U64));
+        assert_eq!(node.item, TypeNode::Number(NumberTypeNode::le(U64)));
+        assert_eq!(node.prefix, NestedTypeNode::Value(NumberTypeNode::le(U8)));
+        assert_eq!(node.fixed, false);
+    }
+
+    #[test]
+    fn fixed() {
+        let node = OptionTypeNode::fixed(NumberTypeNode::le(U64));
+        assert_eq!(node.item, TypeNode::Number(NumberTypeNode::le(U64)));
+        assert_eq!(node.prefix, NestedTypeNode::Value(NumberTypeNode::le(U8)));
+        assert_eq!(node.fixed, true);
+    }
+
+    #[test]
+    fn direct_instanciation() {
+        let node = OptionTypeNode {
+            fixed: true,
+            item: StringTypeNode::utf8().into(),
+            prefix: NumberTypeNode::le(U64).into(),
+        };
+
+        assert_eq!(node.item, TypeNode::String(StringTypeNode::utf8()));
+        assert_eq!(node.prefix, NestedTypeNode::Value(NumberTypeNode::le(U64)));
+        assert_eq!(node.fixed, true);
+    }
+}
--- codama-nodes/src/type_nodes/type_node.rs
+++ codama-nodes/src/type_nodes/type_node.rs
@@ -3,8 +3,8 @@ use codama_nodes_derive::IntoEnum;
 use crate::{
     AmountTypeNode, ArrayTypeNode, BooleanTypeNode, BytesTypeNode, DateTimeTypeNode, EnumTypeNode,
     FixedSizeTypeNode, HiddenPrefixTypeNode, HiddenSuffixTypeNode, MapTypeNode, NumberTypeNode,
-    PostOffsetTypeNode, PreOffsetTypeNode, SetTypeNode, SolAmountTypeNode, StringTypeNode,
-    StructTypeNode, TupleTypeNode, TypeNodeEnumTrait,
+    OptionTypeNode, PostOffsetTypeNode, PreOffsetTypeNode, SetTypeNode, SolAmountTypeNode,
+    StringTypeNode, StructTypeNode, TupleTypeNode, TypeNodeEnumTrait,
 };
 
 #[derive(IntoEnum, Debug, PartialEq)]
@@ -20,6 +20,7 @@ pub enum TypeNode {
     HiddenSuffix(Box<HiddenSuffixTypeNode<TypeNode>>),
     Map(Box<MapTypeNode>),
     Number(NumberTypeNode),
+    Option(Box<OptionTypeNode>),
     PostOffset(Box<PostOffsetTypeNode<TypeNode>>),
     PreOffset(Box<PreOffsetTypeNode<TypeNode>>),
     Set(Box<SetTypeNode>),
