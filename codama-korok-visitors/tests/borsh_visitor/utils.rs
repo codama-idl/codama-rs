a0b3d167a6d62636a3dc62ebce4ff6f822432d19
Loris Leiva <loris.leiva@gmail.com>
2024-11-28 18:31:15 +0000

    Refactor test helpers

--- codama-korok-visitors/tests/borsh_visitor/utils.rs
+++ codama-korok-visitors/tests/borsh_visitor/utils.rs
@@ -4,29 +4,27 @@ use codama_nodes::Node;
 use proc_macro2::TokenStream;
 use quote::quote;
 
-pub fn get_node_from_item(tt: TokenStream) -> Option<Node> {
+pub fn get_node(tt: TokenStream, node_getter: fn(RootKorok) -> Option<Node>) -> Option<Node> {
     let store = RootStore::populate_from(tt).unwrap();
     let mut korok = RootKorok::parse(&store).unwrap();
     korok.accept(&mut BorshVisitor::new());
-    korok.first_item().node()
+    node_getter(korok)
+}
+
+pub fn get_node_from_item(tt: TokenStream) -> Option<Node> {
+    get_node(tt, |k| k.first_item().node())
 }
 
 pub fn get_node_from_enum_variant(tt: TokenStream) -> Option<Node> {
-    let store = RootStore::populate_from(quote! { pub enum Foo { #tt } }).unwrap();
-    let mut korok = RootKorok::parse(&store).unwrap();
-    korok.accept(&mut BorshVisitor::new());
-    match korok.first_item() {
+    get_node(quote! { pub enum Foo { #tt } }, |k| match k.first_item() {
         ItemKorok::Enum(k) => k.variants[0].node.clone(),
         _ => None,
-    }
+    })
 }
 
 pub fn get_node_from_type(tt: TokenStream) -> Option<Node> {
-    let store = RootStore::populate_from(quote! { pub struct Foo(#tt); }).unwrap();
-    let mut korok = RootKorok::parse(&store).unwrap();
-    korok.accept(&mut BorshVisitor::new());
-    match korok.first_item() {
+    get_node(quote! { pub struct Foo(#tt); }, |k| match k.first_item() {
         ItemKorok::Struct(k) => k.fields.all[0].node.clone(),
         _ => None,
-    }
+    })
 }
