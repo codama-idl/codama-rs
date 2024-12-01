75735856174bb78482345d53f9d0d2c4c7b6127d
Loris Leiva <loris.leiva@gmail.com>
2024-12-01 09:42:56 +0000

    Add enum tests for BottomUpVisitor

---
+++ codama-korok-visitors/tests/bottom_up_visitor/enum_korok.rs
@@ -0,0 +1,68 @@
+use codama_korok_visitors::{BottomUpVisitor, KorokVisitable};
+use codama_koroks::EnumKorok;
+use codama_nodes::{
+    DefinedTypeNode, EnumEmptyVariantTypeNode, EnumStructVariantTypeNode, EnumTupleVariantTypeNode,
+    EnumTypeNode, Node, NumberTypeNode, StringTypeNode, StructFieldTypeNode, StructTypeNode,
+    TupleTypeNode, I32,
+};
+use codama_syn_helpers::syn_build;
+use quote::quote;
+
+#[test]
+fn it_creates_a_defined_type_enum_from_variants() {
+    let ast: syn::ItemEnum = syn_build::parse(quote! {
+        enum Message {
+            Quit,
+            Move { x: i32, y: i32 },
+            Write(String),
+        }
+    });
+    let mut korok = EnumKorok::parse(&ast).unwrap();
+    let variant_quit = EnumEmptyVariantTypeNode::new("quit");
+    let variant_move = EnumStructVariantTypeNode::new(
+        "move",
+        StructTypeNode::new(vec![
+            StructFieldTypeNode::new("x", NumberTypeNode::le(I32)),
+            StructFieldTypeNode::new("y", NumberTypeNode::le(I32)),
+        ]),
+    );
+    let variant_write = EnumTupleVariantTypeNode::new(
+        "write",
+        TupleTypeNode::new(vec![StringTypeNode::utf8().into()]),
+    );
+    korok.variants[0].node = Some(variant_quit.clone().into());
+    korok.variants[1].node = Some(variant_move.clone().into());
+    korok.variants[2].node = Some(variant_write.clone().into());
+
+    assert_eq!(korok.node, None);
+    korok.accept(&mut BottomUpVisitor::new());
+    assert_eq!(
+        korok.node,
+        Some(
+            DefinedTypeNode::new(
+                "message",
+                EnumTypeNode::new(vec![
+                    variant_quit.into(),
+                    variant_move.into(),
+                    variant_write.into(),
+                ])
+            )
+            .into()
+        )
+    );
+}
+
+#[test]
+fn it_does_not_override_existing_nodes_by_default() {
+    let ast: syn::ItemEnum = syn_build::parse(quote! { enum Direction { Left } });
+    let mut korok = EnumKorok::parse(&ast).unwrap();
+    korok.variants[0].node = Some(EnumEmptyVariantTypeNode::new("left").into());
+
+    let original_node = Some(Node::from(DefinedTypeNode::new(
+        "direction",
+        EnumTypeNode::new(vec![EnumEmptyVariantTypeNode::new("right").into()]),
+    )));
+    korok.node = original_node.clone();
+    korok.accept(&mut BottomUpVisitor::new());
+    assert_eq!(korok.node, original_node);
+}
--- codama-korok-visitors/tests/bottom_up_visitor/mod.rs
+++ codama-korok-visitors/tests/bottom_up_visitor/mod.rs
@@ -1,3 +1,4 @@
+mod enum_korok;
 mod enum_variant_korok;
 mod field_korok;
 mod fields_korok;
