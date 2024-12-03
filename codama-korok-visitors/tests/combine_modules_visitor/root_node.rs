4992340060ec6cb9ff482a40af525d3e2ad10685
Loris Leiva <loris.leiva@gmail.com>
2024-12-03 09:59:50 +0000

    Add visitor tests

--- codama-korok-visitors/tests/combine_modules_visitor/root_node.rs
+++ codama-korok-visitors/tests/combine_modules_visitor/root_node.rs
@@ -1,5 +1,5 @@
 use super::utils::{combine_modules, CombineModulesInput};
-use codama_nodes::{ProgramNode, RootNode};
+use codama_nodes::{DefinedTypeNode, NumberFormat::U32, NumberTypeNode, ProgramNode, RootNode};
 
 #[test]
 fn default_roots() {
@@ -35,6 +35,39 @@ fn roots_with_same_pubkey_programs() {
                 public_key: "1234".into(),
                 ..Default::default()
             })
+            .into()
+        )
+    );
+}
+
+#[test]
+fn defined_root_within_scraps() {
+    assert_eq!(
+        combine_modules(
+            CombineModulesInput::new()
+                .add_node(DefinedTypeNode::new("foo", NumberTypeNode::le(U32)))
+                .add_node(RootNode::new(ProgramNode {
+                    name: "my_program".into(),
+                    public_key: "1234".into(),
+                    ..Default::default()
+                }))
+                .add_node(DefinedTypeNode::new("bar", NumberTypeNode::le(U32)))
+        ),
+        Some(
+            RootNode {
+                program: ProgramNode {
+                    name: "my_program".into(),
+                    public_key: "1234".into(),
+                    ..Default::default()
+                },
+                additional_programs: vec![ProgramNode {
+                    defined_types: vec![
+                        DefinedTypeNode::new("foo", NumberTypeNode::le(U32)),
+                        DefinedTypeNode::new("bar", NumberTypeNode::le(U32))
+                    ],
+                    ..Default::default()
+                }]
+            }
             .into()
         )
     );
