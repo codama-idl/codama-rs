02dcbd1a29f52f8c5939c4e6101e9ca669ef6c9f
Loris Leiva <loris.leiva@gmail.com>
2024-12-04 17:20:36 +0000

    Add FilterItemsVisitor

---
+++ codama-korok-visitors/src/filter_items_visitor.rs
@@ -0,0 +1,26 @@
+use crate::KorokVisitor;
+use codama_koroks::ItemKorok;
+
+pub struct FilterItemsVisitor<'a> {
+    pub filter: fn(item: &ItemKorok) -> bool,
+    pub visitor: Box<dyn KorokVisitor + 'a>,
+}
+
+impl<'a> FilterItemsVisitor<'a> {
+    pub fn new<T: KorokVisitor + 'a>(filter: fn(item: &ItemKorok) -> bool, visitor: T) -> Self {
+        Self {
+            filter,
+            visitor: Box::new(visitor),
+        }
+    }
+}
+
+impl<'a> KorokVisitor for FilterItemsVisitor<'a> {
+    fn visit_item(&mut self, korok: &mut codama_koroks::ItemKorok) {
+        if (self.filter)(korok) {
+            self.visitor.visit_item(korok);
+        } else {
+            self.visit_children(korok);
+        }
+    }
+}
---
+++ codama-korok-visitors/tests/filter_items_visitor.rs
@@ -0,0 +1,65 @@
+use codama_korok_visitors::{FilterItemsVisitor, KorokVisitable, MapVisitor};
+use codama_koroks::ItemKorok;
+use codama_nodes::PublicKeyTypeNode;
+use codama_syn_helpers::syn_build;
+use quote::quote;
+
+#[test]
+fn it_only_starts_the_child_visitor_on_filtered_items() {
+    let ast: syn::Item = syn_build::parse(quote! {
+        mod parent {
+            mod foo {
+                pub struct Foo(u32);
+            }
+            mod bar {
+                pub struct Bar(u32);
+            }
+        }
+    });
+    let mut korok = ItemKorok::parse(&ast, &[], &mut 0).unwrap();
+
+    korok.accept(&mut FilterItemsVisitor::new(
+        |item| match item {
+            ItemKorok::Module(module) => module.ast.ident == "foo",
+            _ => false,
+        },
+        MapVisitor::new(|k| k.set_node(Some(PublicKeyTypeNode::new().into()))),
+    ));
+
+    let ItemKorok::Module(module) = &korok else {
+        panic!("Expected parent module");
+    };
+    let ItemKorok::Module(foo) = &module.items[0] else {
+        panic!("Expected foo module");
+    };
+    let ItemKorok::Struct(foo_struct) = &foo.items[0] else {
+        panic!("Expected foo struct");
+    };
+    let ItemKorok::Module(bar) = &module.items[1] else {
+        panic!("Expected bar module");
+    };
+    let ItemKorok::Struct(bar_struct) = &bar.items[0] else {
+        panic!("Expected bar struct");
+    };
+
+    assert_eq!(foo.node, Some(PublicKeyTypeNode::new().into()));
+    assert_eq!(foo_struct.node, Some(PublicKeyTypeNode::new().into()));
+    assert_eq!(
+        foo_struct.fields.node,
+        Some(PublicKeyTypeNode::new().into())
+    );
+    assert_eq!(
+        foo_struct.fields.all[0].node,
+        Some(PublicKeyTypeNode::new().into())
+    );
+    assert_eq!(
+        foo_struct.fields.all[0].r#type.node,
+        Some(PublicKeyTypeNode::new().into())
+    );
+
+    assert_eq!(bar.node, None);
+    assert_eq!(bar_struct.node, None);
+    assert_eq!(bar_struct.fields.node, None);
+    assert_eq!(bar_struct.fields.all[0].node, None);
+    assert_eq!(bar_struct.fields.all[0].r#type.node, None);
+}
