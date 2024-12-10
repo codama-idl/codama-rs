0e40bbc60d1446fec00ad65ff7abb5a866939580
Loris Leiva <loris.leiva@gmail.com>
2024-12-10 11:16:40 +0000

    Remove internal derive tests

--- codama-nodes/derive/tests/mod.rs
+++ codama-nodes/derive/tests/mod.rs
@@ -6,5 +6,4 @@ fn tests() {
     t.pass("tests/node_union/*.rs");
     t.pass("tests/registered_nodes/*.pass.rs");
     t.compile_fail("tests/registered_nodes/*.fail.rs");
-    t.pass("tests/type_node/*.rs");
 }
