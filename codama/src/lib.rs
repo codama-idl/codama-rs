89d458096d3e56a6aee6e2bd894cb4884d9257b7
Loris Leiva <loris.leiva@gmail.com>
2025-09-22 15:02:11 +0100

    Export only codama-macros from codama in Solana targets (#28)

--- codama/src/lib.rs
+++ codama/src/lib.rs
@@ -1,12 +1,14 @@
+pub use codama_macros::*;
+
+// Solana programs only need Codama macros, which expand to
+// nothing at compile time. Everything else must be excluded
+// to avoid compiling non-Solana-compatible code.
+
+#[cfg(not(target_os = "solana"))]
 mod codama;
 
-pub use codama::*;
-pub use codama_errors::*;
-pub use codama_korok_plugins::*;
-pub use codama_korok_visitors::*;
-pub use codama_koroks::*;
-pub use codama_nodes::*;
-pub use codama_stores::*;
-
-extern crate codama_macros;
-pub use codama_macros::*;
+#[cfg(not(target_os = "solana"))]
+pub use {
+    codama::*, codama_errors::*, codama_korok_plugins::*, codama_korok_visitors::*,
+    codama_koroks::*, codama_nodes::*, codama_stores::*,
+};
