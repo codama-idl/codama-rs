4d6d9649d34f9e667adf4642f5cbde444f51b493
Loris Leiva <loris.leiva@gmail.com>
2025-10-15 13:51:31 +0100

    Add MetaConsumer trait for partial meta parsing

---
+++ codama-attributes/src/codama_directives/type_nodes/struct_field_meta_consumer.rs
@@ -0,0 +1,92 @@
+use crate::utils::{FromMeta, MetaConsumer, SetOnce};
+use codama_nodes::{
+    CamelCaseString, DefaultValueStrategy, InstructionInputValueNode, TypeNode, ValueNode,
+};
+use codama_syn_helpers::Meta;
+
+pub(crate) struct StructFieldMetaConsumer {
+    pub metas: Vec<Meta>,
+    pub name: SetOnce<CamelCaseString>,
+    pub r#type: SetOnce<TypeNode>,
+    pub default_value: SetOnce<ValueNode>,
+    #[allow(dead_code)]
+    pub argument_default_value: SetOnce<InstructionInputValueNode>,
+    pub default_value_strategy: SetOnce<DefaultValueStrategy>,
+}
+
+impl MetaConsumer for StructFieldMetaConsumer {
+    fn new(metas: Vec<Meta>) -> Self {
+        Self {
+            metas,
+            name: SetOnce::new("name"),
+            r#type: SetOnce::new("type"),
+            default_value: SetOnce::new("default_value"),
+            argument_default_value: SetOnce::new("default_value"),
+            default_value_strategy: SetOnce::new("default_value_strategy"),
+        }
+    }
+
+    fn metas(&self) -> &[Meta] {
+        &self.metas
+    }
+
+    fn metas_mut(&mut self) -> &mut Vec<Meta> {
+        &mut self.metas
+    }
+}
+
+impl StructFieldMetaConsumer {
+    pub fn consume_field(self) -> syn::Result<Self> {
+        self.consume_metas(|this, meta| match meta.path_str().as_str() {
+            "name" => {
+                this.name.set(String::from_meta(&meta)?.into(), meta)?;
+                Ok(None)
+            }
+            "type" => {
+                let node = TypeNode::from_meta(&meta.as_path_value()?.value)?;
+                this.r#type.set(node, meta)?;
+                Ok(None)
+            }
+            "default_value_omitted" => {
+                meta.as_path()?;
+                this.default_value_strategy
+                    .set(DefaultValueStrategy::Omitted, meta)?;
+                Ok(None)
+            }
+            _ => {
+                if let Ok(value) = String::from_meta(&meta) {
+                    this.name.set(value.into(), meta)?;
+                    return Ok(None);
+                }
+                if let Ok(node) = TypeNode::from_meta(&meta) {
+                    this.r#type.set(node, meta)?;
+                    return Ok(None);
+                }
+                Ok(Some(meta))
+            }
+        })
+    }
+
+    pub fn consume_default_value(self) -> syn::Result<Self> {
+        self.consume_metas(|this, meta| match meta.path_str().as_str() {
+            "default_value" => {
+                let node = ValueNode::from_meta(&meta.as_path_value()?.value)?;
+                this.default_value.set(node, meta)?;
+                Ok(None)
+            }
+            _ => Ok(Some(meta)),
+        })
+    }
+
+    #[allow(dead_code)]
+    pub fn consume_argument_default_value(self) -> syn::Result<Self> {
+        self.consume_metas(|this, meta| match meta.path_str().as_str() {
+            "default_value" => {
+                let node = InstructionInputValueNode::from_meta(&meta.as_path_value()?.value)?;
+                this.argument_default_value.set(node, meta)?;
+                Ok(None)
+            }
+            _ => Ok(Some(meta)),
+        })
+    }
+}
--- codama-attributes/src/utils/mod.rs
+++ codama-attributes/src/utils/mod.rs
@@ -1,6 +1,8 @@
 mod from_meta;
 mod macros;
+mod meta_consumer;
 mod set_once;
 
 pub use from_meta::*;
+pub use meta_consumer::*;
 pub use set_once::*;
